86f2ee78b63ed21a11e46a7d6c798bbd8029f4de
Sergei Volkov <Sneedjik@gmail.com>
2024-09-18 01:15:10 +0600

    completed Repository search

---
+++ src/test/java/SelenideRepositorySearchTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
+
+public class SelenideRepositorySearchTest {
+
+
+    @Test
+    void shouldFindSelenideRepositoryAtTheTop() {
+
+        open("https://github.com/");
+        $(".header-search-button").click();
+        $("#query-builder-test").setValue("selenide").pressEnter();
+        $$("[data-testid=results-list] div").first().$("a").click();
+        $("#repository-container-header").shouldHave(text("selenide / selenide"));
+    }
+}

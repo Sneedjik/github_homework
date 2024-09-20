6ccff2dafc830addb97ab72178f61aa2559a0a71
Sergei Volkov <Sneedjik@gmail.com>
2024-09-21 01:07:28 +0600

    Reformed Code

--- src/test/java/SelenideRepositorySearchTest.java
+++ src/test/java/SelenideRepositorySearchTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideRepositorySearchTest {
 
